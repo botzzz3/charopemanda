5d2addd0ffed96ad62566fe9735c1ed9c6eb08f2
botzzz3 <77111954+botzzz3@users.noreply.github.com>
2021-03-06 11:45:29 -0300

    Add files via upload

---
+++ src/charope.js
@@ -0,0 +1,12 @@
+const charope = (prefix, sender) => {
+	return `Titulo
+`
+  🅢︎🅔︎🅙︎🅐︎ 🅑︎🅔︎🅜︎ 🅥︎🅘︎🅝︎🅓︎🅞︎(🅐︎)
+  𝗰𝗿𝗶𝗮𝗱𝗼𝗿 𝗱𝗼 𝗯𝗼𝘁 𝗰𝗵𝗮𝗿𝗼𝗽𝗲_𝗺𝗼𝗱𝘇
+  𝗻𝘂𝗺𝗲𝗿𝗼 🄳🄴🄻🄴 Wa.me/447520621694
+  ---------------------------------
+  ᴄᴏᴍᴀɴᴅᴏs ᴜsᴇ (!) ᴘᴀʀᴀ ᴄᴀᴅᴀ ᴄᴏᴍᴀɴᴅᴏ
+  🔰{prefix}menuadmin'
+}
+
+exports.charope = charope
